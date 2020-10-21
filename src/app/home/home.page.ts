bf3b22c3715233a0ba84c490218242c5d36e5c59
Warokk <theo.uzan@hotmail.fr>
2020-10-21 14:34:55 +0200

    added android config

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,14 +1,8 @@
 import { Component } from '@angular/core';
-import { NavController } from "ionic-angular";
-import { ContactPage } from './header/header.page.ts';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
-
-  constructor(public navCtrl: NavController, public contactPage: ContactPage) {}
-
-}
+export class HomePage {}
